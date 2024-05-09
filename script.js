0f59abe8bccf06d8b74400187141be209a5d8009
iamankusingh <ankusingh@gmail.com>
2024-05-09 12:31:08 +0530

    responsive

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // parallas scrolling wibsite
 let hill1 = document.getElementById("hill1");
 let hill4 = document.getElementById("hill4");
+let imgHeight = document.getElementById("hill4").height;
 let hill5 = document.getElementById("hill5");
 let leaf = document.getElementById("leaf");
 
@@ -13,3 +14,11 @@ document.addEventListener("scroll", () => {
   leaf.style.top = value * 1 + "px";
   leaf.style.left = value * 1 + "px";
 });
+
+function sizeSection() {
+  let main = document.querySelector("main");
+  main.style.height = `${imgHeight}px`;
+  console.log("resize");
+}
+
+document.addEventListener("resize", sizeSection());
